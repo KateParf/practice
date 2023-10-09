3f1fc432d7bc12f4283a8f29c1e65f1993c8f847
Gilmutdin <145389273+Gilmutdin@users.noreply.github.com>
2023-10-09 17:44:19 +0400

    Add files via upload
    Добавлен класс SqrFunction

--- LR2/functions/src/main/java/functions/SqrFunction.java
+++ LR2/functions/src/main/java/functions/SqrFunction.java
@@ -1,7 +1,9 @@
 package functions;
 
-public class SqrFunction implements MathFunction {
-    public double apply(double x) {
+public class SqrFunction implements MathFunction
+{
+    public double apply(double x)
+    {
         return Math.pow(x,2);
     }
 }
