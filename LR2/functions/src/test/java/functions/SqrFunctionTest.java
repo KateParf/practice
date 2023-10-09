c596d9c5b255eda58417245ab119419a9ae062c8
Gilmutdin <145389273+Gilmutdin@users.noreply.github.com>
2023-10-09 18:00:55 +0400

    Добавлен тест для класса SqrFunction

--- LR2/functions/src/test/java/functions/SqrFunctionTest.java
+++ LR2/functions/src/test/java/functions/SqrFunctionTest.java
@@ -1,29 +1,32 @@
 package functions;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.api.BeforeEach;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SqrFunctionTest {
-
-    private SqrFunction test;
-
+public class SqrFunctionTest
+{
+    SqrFunction test;
     @BeforeEach
-    void setUp()  {
+    void testCreate()
+    {
         test = new SqrFunction();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "1, 1",
-            "0.5, 0.25",
-            "-2, 4",
-            "-0.01, 0.0001"}, ignoreLeadingAndTrailingWhitespace = true)
-    void testApply(double funk, double cor) {
-        assertEquals(test.apply(funk), cor);
+    @CsvSource
+            (value = {
+                "1, 1",
+                "0.5, 0.25",
+                "-2, 4",
+                "10, 100"
+            })
+
+    void check(double orig, double calc)
+    {
+        assertEquals(test.apply(orig), calc);
     }
 }
